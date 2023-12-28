c361dfd1d5af9c5de89eff09dd657b9bc84bee60
hamed gh <hamedgh3285@gmail.com>
2023-12-28 11:56:24 +0330

    update css and js in resources

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,19 @@
+window._ = require('lodash');
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
